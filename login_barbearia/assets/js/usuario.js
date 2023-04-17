031a6b352966e1a15d3a21bff4e5d26c65ec61a3
JhonDSM <100415194+JhonDSM@users.noreply.github.com>
2023-04-16 21:19:16 -0300

    Update usuario.js

--- login_barbearia/assets/js/usuario.js
+++ login_barbearia/assets/js/usuario.js
@@ -1,8 +1,8 @@
 // Classe Usuario
 class Usuario {
-    constructor(nome, senha, nivelAcesso) {
-      this.nome = nome;
-      this.senha = senha;
-      this.nivelAcesso = nivelAcesso;
-    }
+  constructor(nome, senha, nivelAcesso) {
+    this.nome = nome;
+    this.senha = senha;
+    this.nivelAcesso = nivelAcesso;
   }
+}
